716aad9a85f7b9b3cbbf5af0230e95f4d132a2ab
XiansongLi <iametkai@gmail.com>
2019-02-25 09:02:32 -0800

    the thrid version

--- 360hw2/src/cse360assign2/Calculator.java
+++ 360hw2/src/cse360assign2/Calculator.java
@@ -3,9 +3,10 @@ package cse360assign2;
 public class Calculator 
 {
 	private int total;
-
+	private String history;
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
+		history = "0";
 	}
 	/** the getTotal method return the current total*/
 	public int getTotal () 
@@ -16,16 +17,19 @@ public class Calculator
 	public void add (int value) 
 	{
 		total += value; 
+		history = history + " + "+ value;
 	}
 	/**the subtract method subtract the parameter from the total variable*/
 	public void subtract (int value) 
 	{
 		total -= value;
+		history = history + " - "+ value;
 	}
 	/**the multiply method multiply the total by the parameter*/
 	public void multiply (int value) 
 	{
 		total *= value;
+		history = history + " * "+ value;
 	}
 	/**the divide method divide the total by the parameter*/
 	public void divide (int value) 
@@ -38,10 +42,26 @@ public class Calculator
 		{ 
 			total /= value;  
 		}
+		history = history + " / "+ value;
 	}
-	
+	/**the getHistory class will return a history of all actions as a string*/
 	public String getHistory ()  
 	{
-		return "";   
+		return history;   
+	}
+}
+
+class test
+{
+	public static void main(String[] args)
+	{
+		
+		Calculator ca=new Calculator();
+		ca.add(4);
+		ca.subtract(2);
+		ca.multiply(2);
+		ca.add(5);
+		System.out.println(ca.getTotal());
+		System.out.print(ca.getHistory());
 	}
 }
