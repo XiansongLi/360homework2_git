51352e8d3c174f6d1d14bee340ef82c3cd96d1b2
XiansongLi <47956682+XiansongLi@users.noreply.github.com>
2019-02-25 19:25:54 -0700

    Add files via upload

---
+++ 360hw2.1/360hw2_second/src/cse360assign2/Calculator.java
@@ -0,0 +1,41 @@
+package cse360assign2;
+/**the first version*/
+public class Calculator 
+{
+	private int total;
+
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	/** the getTotal method return the current total*/
+	public int getTotal () 
+	{
+		return 0;
+	}
+	/**the add method add the parameter to the total variable*/
+	public void add (int value) 
+	{
+		
+	}
+	/**the subtract method subtract the parameter from the total variable*/
+	public void subtract (int value) 
+	{
+		
+	}
+	/**the multiply method multiply the total by the parameter*/
+	public void multiply (int value) 
+	{
+		
+	}
+	/**the divide method divide the total by the parameter*/
+	public void divide (int value) 
+	{
+
+	}
+	public String getHistory ()  
+	{
+		return "";   
+	}
+
+}
+ 
